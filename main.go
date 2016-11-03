1886e4a8f9c5adbe2855a522ac19fc44c07d4c16
Kumar Gaurav <kgthegreat@gmail.com>
2016-11-03 16:54:56 +0800

    More countries

--- main.go
+++ main.go
@@ -25,6 +25,11 @@ func NewServer(addr string) *http.Server {
 	initStaticRouting()
 
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hongkong", hkCCHandler)
+	http.HandleFunc("/singapore", sgCCHandler)
+	http.HandleFunc("/taiwan", twCCHandler)
+	http.HandleFunc("/portugal", ptCCHandler)
+	http.HandleFunc("/hackathon", hackCCHandler)
 	http.HandleFunc("/robots.txt", robotsHandler)
 	//http.HandleFunc("/compare", comparehandler)
 	// Create and start server
@@ -50,12 +55,67 @@ func robotsHandler(w http.ResponseWriter, req *http.Request) {
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
 
+	var empty []string
+	renderTemplate(w, "index", empty)
+
+}
+
+func hkCCHandler(w http.ResponseWriter, req *http.Request) {
+
 	siteName := "https://www.moneyhero.com.hk"
 	url := getUrl(siteName)
 	resp := getResponse(url)
 	varmap := getVarmap(resp)
 	defer resp.Body.Close()
 
-	renderTemplate(w, "index", varmap)
+	renderTemplate(w, "cc", varmap)
+
+}
+
+func sgCCHandler(w http.ResponseWriter, req *http.Request) {
+
+	siteName := "https://www.singsaver.com.sg"
+	url := getUrl(siteName)
+	resp := getResponse(url)
+	varmap := getVarmap(resp)
+	defer resp.Body.Close()
+
+	renderTemplate(w, "cc", varmap)
+
+}
+
+func twCCHandler(w http.ResponseWriter, req *http.Request) {
+
+	siteName := "https://www.money101.com.tw"
+	url := getUrl(siteName)
+	resp := getResponse(url)
+	varmap := getVarmap(resp)
+	defer resp.Body.Close()
+
+	renderTemplate(w, "cc", varmap)
+
+}
+
+func ptCCHandler(w http.ResponseWriter, req *http.Request) {
+
+	siteName := "https://www.comparaja.pt"
+	url := getUrl(siteName)
+	resp := getResponse(url)
+	varmap := getVarmap(resp)
+	defer resp.Body.Close()
+
+	renderTemplate(w, "cc", varmap)
+
+}
+
+func hackCCHandler(w http.ResponseWriter, req *http.Request) {
+
+	siteName := "https://hackathon.compareglobal.co.uk"
+	url := getUrl(siteName)
+	resp := getResponse(url)
+	varmap := getVarmap(resp)
+	defer resp.Body.Close()
+
+	renderTemplate(w, "cc", varmap)
 
 }
